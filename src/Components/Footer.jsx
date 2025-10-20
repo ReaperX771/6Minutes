7bd2b8a0df1653c949df8986594e0cdebd8f9c25
ReaperX771 <unknowngrand53@gmail.com>
2025-10-20 13:00:26 +0100

    favi added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ import log from '../assets/images/log.jpg'
 const Footer = () => {
   return (
     <footer className="bg-black text-white py-8">
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center px-4">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between gap-7 items-center px-4">
         <img src={log} alt="6Minutes Logo" className="h-10 w-auto" />
         <div className="flex gap-4 mb-4 md:mb-0">
           <a href="#" className="hover:text-red-600 transition"><FaTwitter size={24} /></a>
